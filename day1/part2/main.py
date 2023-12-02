2b929d9737998af113e1aa6667131e93a09bee0b
Ciaran Concannon <cjcon90@pm.me>
2023-12-02 17:41:59 +0000

    formatting || delete duplicate code

--- day1/part2/main.py
+++ day1/part2/main.py
@@ -15,19 +15,6 @@ def _word_to_num(line: str) -> str:
     return line
 
 
-WORDNUMS = {'one': '1', 'two': '2', 'three': '3', 'four': '4',
-            'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
-WORDREG = re.compile('|'.join(WORDNUMS.keys()))
-ALLREG = re.compile('|'.join(list(WORDNUMS.keys()) +
-                    list(str(num) for num in WORDNUMS.values())))
-
-
-def _word_to_num(line: str) -> str:
-    for word in WORDREG.findall(line):
-        line = line.replace(word, str(WORDNUMS[word]))
-    return line
-
-
 def main():
     total = 0
     p = Path(__file__).with_name('input.txt')
